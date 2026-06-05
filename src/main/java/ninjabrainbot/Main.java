6f3fb0486f69ac27518fde6f760234369a763782
ImWeen <rxtf69@gmail.com>
2026-06-05 09:31:32 +0300

    Evil Ninjabrain Bot initial edits commit

--- src/main/java/ninjabrainbot/Main.java
+++ src/main/java/ninjabrainbot/Main.java
@@ -17,7 +17,7 @@ import ninjabrainbot.util.Profiler;
 
 public class Main {
 
-	public static final String VERSION = "1.5.2";
+	public static final String VERSION = "6.7.0";
 
 	public static void main(String[] args) {
 		ErrorHandler errorHandler = new ErrorHandler();
